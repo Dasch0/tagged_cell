13eb59afb7581a55b74a9509506c52642a4e73f3
David Schwarz <dsdavidschwarz@gmail.com>
2022-03-13 16:17:05 -0500

    fix safety of new, bump docs and rust version

--- README.md
+++ README.md
@@ -5,20 +5,31 @@
 
 Fast, initializable, and thread safe static variables
 
+Borrows the [excellent ZST based tagging implementation](https://www.hardmo.de/article/2021-03-14-zst-proof-types.md#proof-of-work)
+ to guarantee cells
+are initialized exactly once before an access is attempted.
 
-## Overview
-Borrows the excellent ZST based tagging implementation (linked below) to guarantee the cell
-is initialized exactly once before an access is attempted.
-<https://www.hardmo.de/article/2021-03-14-zst-proof-types.md#proof-of-work>
+This is implemented via the [TaggedCell] and a Tag type, which **must** be unique for
+each instance of the [TaggedCell] for safe operation. The [TaggedCell] must then be set up 
+via [init()][TaggedCell::init], which initializes the underlying data using a user provided function or closure,
+and then returns a special zero-sized [Init] tag used to access the Cell's data.
 
-This is implemented via `TaggedCell` and a Tag type, which **must** be unique for
-each instance of the `TaggedCell` for safe operation. The `TaggedCell` must then be initialized
-via `TaggedCell::init()`, which initializes the underlying data using a user provided function or closure,
-and then returns a special zero-sized `Init<Tag>` used to access the Cell's data.
+```
+use tagged_cell::TaggedCell;
+struct FooTag;
+static FOO: TaggedCell<usize, FooTag> = unsafe {TaggedCell::new()};
 
-To ensure unique tag types are used for each cell, the tagged\_cell! macro is provided. The
+// Initialize the cell's data and retrieve a tag
+let tag = FOO.init(|| 27);
+
+// The tag is required to obtain a shared reference to the data
+assert_eq!(*FOO.get(tag), 27);
+
+```
+
+To ensure unique tag types are used for each cell, and to 'wrap' the unsafe call, the [tagged_cell!] macro is provided. The
 macro creates a new tag type based on the variable's name, and applies it in the declaration.
-```rust
+```
 use tagged_cell::tagged_cell;
 tagged_cell!{
    static BAR: TaggedCell<Vec<usize>, _> = TaggedCell::new();
@@ -30,10 +41,31 @@ let vec = BAR.get(tag);
 assert_eq!(vec[2], 20);
 ```
 
-To allow for usage across threads, only the first invocation of TaggedCell::init will initialize the
-Cell's data. All future TaggedCell::init calls will just return a new tag. It is undetermined which
+When unique tag types are used, attempting to access a [TaggedCell] before it is initialized
+will cause a compilation error.
+```compile_fail
+use tagged_cell::tagged_cell;
+
+tagged_cell!{
+    static BAZ: TaggedCell<usize>, _> = TaggedCell::new();
+}
+tagged_cell!{
+    static QUX: TaggedCell<usize>, _> = TaggedCell::new();
+}
+
+// read before init is not possible
+BAZ.get(Init{BAZ::TagType});
+
+let qux_tag = QUX.init(|| 35);
+
+// using the wrong tag throws an error
+BAZ.get(qux_tag);
+```
+
+To allow for usage across threads, only the first invocation of [init()][TaggedCell::init] will initialize the
+Cell's data. All future [init()][TaggedCell::init] calls will just return a new tag. It is undetermined which
 thread will initialize the Cell's data.
-```rust
+```
 use std::thread;
 use tagged_cell::tagged_cell;
 
--- src/lib.rs
+++ src/lib.rs
@@ -1,90 +1,8 @@
-//! Fast, initializable, and thread safe static variables
-//!
-//! Borrows the excellent ZST based tagging implementation (linked below) to guarantee the cell
-//! is initialized exactly once before an access is attempted.
-//! <https://www.hardmo.de/article/2021-03-14-zst-proof-types.md#proof-of-work>
-//!
-//! This is implemented via the [TaggedCell] and a Tag type, which **must** be unique for
-//! each instance of the [TaggedCell] for safe operation. The [TaggedCell] must then be initialized
-//! via [init()][TaggedCell::init], which initializes the underlying data using a user provided function or closure,
-//! and then returns a special zero-sized [Init] tag used to access the Cell's data.
-//! ```
-//! use tagged_cell::TaggedCell;
-//! struct FooTag;
-//! static FOO: TaggedCell<usize, FooTag> = TaggedCell::new();
-//!
-//! // Initialize the cell's data and retrieve a tag
-//! let tag = FOO.init(|| 27);
-//!
-//! // The tag is required to obtain a shared reference to the data
-//! assert_eq!(*FOO.get(tag), 27);
-//!
-//! ```
-//!
-//! To ensure unique tag types are used for each cell, the [tagged_cell!] macro is recommended. The
-//! macro creates a new tag type based on the variable's name, and applies it in the declaration.
-//! ```
-//! use tagged_cell::tagged_cell;
-//! tagged_cell!{
-//!    static BAR: TaggedCell<Vec<usize>, _> = TaggedCell::new();
-//! }
-//!
-//! let tag = BAR.init(|| vec![0, 10, 20]);
-//! let vec = BAR.get(tag);
-//!
-//! assert_eq!(vec[2], 20);
-//! ```
-//!
-//! When unique tag types are used, attempting to access a [TaggedCell] before it is initialized
-//! will cause a compilation error.
-//! ```compile_fail
-//! use tagged_cell::tagged_cell;
-//!
-//! tagged_cell!{
-//!     static BAZ: TaggedCell<usize>, _> = TaggedCell::new();
-//! }
-//! tagged_cell!{
-//!     static QUX: TaggedCell<usize>, _> = TaggedCell::new();
-//! }
-//!
-//! // read before init is not possible
-//! BAZ.get(Init{BAZ::TagType});
-//!
-//! let qux_tag = QUX.init(|| 35);
-//!
-//! // using the wrong tag throws an error
-//! BAZ.get(qux_tag);
-//! ```
-//!
-//! To allow for usage across threads, only the first invocation of [init()][TaggedCell::init] will initialize the
-//! Cell's data. All future [init()][TaggedCell::init] calls will just return a new tag. It is undetermined which
-//! thread will initialize the Cell's data.
-//! ```
-//! use std::thread;
-//! use tagged_cell::tagged_cell;
-//!
-//! tagged_cell!{
-//!     static TABLE: TaggedCell<Vec<usize>, _> = TaggedCell::new();
-//! }
-//!
-//! thread::spawn(move || {
-//!     let tag = TABLE.init(|| vec![0, 10, 20]);
-//!     let table = TABLE.get(tag);
-//!     assert_eq!(table[2], 20);
-//! });
-//!
-//! thread::spawn(move || {
-//!     let tag = TABLE.init(|| vec![0, 10, 20]);
-//!     let table = TABLE.get(tag);
-//!     assert_eq!(table[1], 10);
-//! });
-//!
-//! ```
-//!
+#![doc = include_str!("../README.md")]
 use std::{cell::UnsafeCell, marker::PhantomData, mem::MaybeUninit, sync::Once};
 
 /// Top level structure to support initializable and thread safe static variables.
-/// It is recommended to use [tagged_cell!] macro to make this struct
+/// Use [tagged_cell!] macro to make this struct
 pub struct TaggedCell<T, Tag> {
     once: Once,
     tag: PhantomData<Tag>,
@@ -100,9 +18,11 @@ pub struct Init<Tag> {
 }
 
 impl<T, Tag> TaggedCell<T, Tag> {
-    /// Make an uninitialized cell.
+    /// Internal method to create an uninitialized cell. This is an implementation detail and
+    /// *should not* be called directly and thus is listed as unsafe. Use [tagged_cell!] instead
     /// This must only be called once for each `Tag` type.
-    pub const fn new() -> Self {
+    #[doc(hidden)]
+    pub const unsafe fn new() -> Self {
         TaggedCell {
             data: UnsafeCell::new(MaybeUninit::<T>::uninit()),
             tag: PhantomData,
@@ -156,7 +76,7 @@ macro_rules! tagged_cell {
             pub struct TagType;
         }
 
-        static $name: $crate::TaggedCell<$type, $name::TagType> = $crate::TaggedCell::new();
+        static $name: $crate::TaggedCell<$type, $name::TagType> = unsafe {$crate::TaggedCell::new()};
     };
 }
 
